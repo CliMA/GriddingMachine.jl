20e35480f1d47532a197048f2b6a85ef7a96fb39
Documenter.jl <documenter@juliadocs.github.io>
2020-08-27 00:53:40 +0000

    build based on 8ee62e9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GlobalCanopy","page":"Home","title":"GlobalCanopy","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Global canopy datasets","category":"page"},{"location":"#Usage","page":"Home","title":"Usage","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"# not registered for now\nusing Pkg;\nPkg.add(\"https://github.com/CliMA/GlobalCanopy.jl.git\");\n\nusing GlobalCanopy","category":"page"},{"location":"API/#API","page":"API","title":"API","text":"","category":"section"},{"location":"API/","page":"API","title":"API","text":"CurrentModule = GlobalCanopy","category":"page"}]
+}
