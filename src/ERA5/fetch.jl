2d5ffcec7290ece8ec8bb92d15762695a00bff94
Yujie Wang <jesiner@gmail.com>
2021-03-16 14:40:08 -0700

    per year and per item options

--- src/ERA5/fetch.jl
+++ src/ERA5/fetch.jl
@@ -3,7 +3,7 @@
 # Download ERA5 regenerated weather file
 #
 ###############################################################################
-ERA5_LAND_SELECTIONS = [
+ERA5_LAND_ALL = [
             "10m_u_component_of_wind",
             "10m_v_component_of_wind",
             "2m_dewpoint_temperature",
@@ -54,11 +54,34 @@ ERA5_LAND_SELECTIONS = [
             "volumetric_soil_water_layer_2",
             "volumetric_soil_water_layer_3",
             "volumetric_soil_water_layer_4"];
+ERA5_LAND_SELECTION = [
+            "10m_u_component_of_wind",
+            "10m_v_component_of_wind",
+            "2m_dewpoint_temperature",
+            "2m_temperature",
+            "evaporation_from_vegetation_transpiration",
+            "leaf_area_index_high_vegetation",
+            "leaf_area_index_low_vegetation",
+            "skin_temperature",
+            "surface_pressure",
+            "surface_solar_radiation_downwards",];
 ERA5_LAND_TIMES = [
             "00:00", "01:00", "02:00", "03:00", "04:00", "05:00",
             "06:00", "07:00", "08:00", "09:00", "10:00", "11:00",
             "12:00", "13:00", "14:00", "15:00", "16:00", "17:00",
             "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"];
+ERA5_SINGLE_LEVELS_SELECTION =[
+            "mean_surface_direct_short_wave_radiation_flux",
+            "mean_surface_downward_short_wave_radiation_flux",
+            "soil_temperature_level_1",
+            "soil_temperature_level_2",
+            "soil_temperature_level_3",
+            "soil_temperature_level_4",
+            "soil_type",
+            "volumetric_soil_water_layer_1",
+            "volumetric_soil_water_layer_2",
+            "volumetric_soil_water_layer_3",
+            "volumetric_soil_water_layer_4"];
 
 
 
@@ -69,7 +92,7 @@ ERA5_LAND_TIMES = [
                 lon_ind::Int,
                 year::Int = 2020,
                 month::Int = 6;
-                vars::Array{String,1} = ERA5_LAND_SELECTIONS,
+                vars::Array{String,1} = ERA5_LAND_ALL,
                 times::Array{String,1} = ERA5_LAND_TIMES,
                 folder::String = ""
     )
@@ -77,7 +100,7 @@ ERA5_LAND_TIMES = [
                 lat_ind::Int,
                 lon_ind::Int,
                 year::Int;
-                vars::Array{String,1} = ERA5_LAND_SELECTIONS,
+                vars::Array{String,1} = ERA5_LAND_ALL,
                 times::Array{String,1} = ERA5_LAND_TIMES,
                 folder::String = ""
     )
@@ -88,7 +111,7 @@ Download dataset per 1 by 1 degree, given
 - `lon_ind` Longitude index (0 for -180~-179, 180 for -1~0)
 - `year` Which year of data to download, default = 2020
 - `month` Which month of data to download, default = 6 (June)
-- `vars` Variables to download, default is `ERA5_LAND_SELECTIONS`
+- `vars` Variables to download, default is `ERA5_LAND_ALL`
 - `times` Time list to download, default is all 24 hours
 - `folder` Folder to save the downloaded dataset, default is "" (current dir)
 """
@@ -98,11 +121,11 @@ function fetch_ERA5!(
             lon_ind::Int,
             year::Int,
             month::Int;
-            vars::Array{String,1} = ERA5_LAND_SELECTIONS,
+            vars::Array{String,1} = ERA5_LAND_ALL,
             times::Array{String,1} = ERA5_LAND_TIMES,
             folder::String = ""
 )
-    @info "Downloading data for year $(year) month $(month)...";
+    @info "Downloading land data for year $(year) month $(month)...";
 
     # set up CDSAPI_CLIENT
     global CDSAPI_CLIENT;
@@ -119,18 +142,16 @@ function fetch_ERA5!(
 
     # retrieve data if file does not exist
     if !isfile(file_name)
-        #month_len = isleapyear(year) ? NDAYS_LEAP : NDAYS;
-        month_len = [31,29,31,30,31,30,31,31,30,31,30,31];
         CDSAPI_CLIENT.retrieve(
             "reanalysis-era5-land",
             Dict(
-                "format" => "netcdf",
+                "format"   => "netcdf",
                 "variable" => vars,
-                "year" => "$(year)",
-                "month" => "$(month)",
-                "day" => ["$(i)" for i in 1:month_len[month]], #month_days(year, month);
-                "time" => times,
-                "area" => ["$(lat2)", "$(lon1)", "$(lat1)", "$(lon2)"],
+                "year"     => "$(year)",
+                "month"    => "$(month)",
+                "day"      => ["$(i)" for i in 1:month_days(year,month)],
+                "time"     => times,
+                "area"     => ["$(lat2)", "$(lon1)", "$(lat1)", "$(lon2)"],
             ),
             file_name
         );
@@ -147,13 +168,252 @@ function fetch_ERA5!(
             lat_ind::Int,
             lon_ind::Int,
             year::Int;
-            vars::Array{String,1} = ERA5_LAND_SELECTIONS,
+            vars::Array{String,1} = ERA5_LAND_SELECTION,
             times::Array{String,1} = ERA5_LAND_TIMES,
             folder::String = ""
 )
-    for month in 1:12
-        fetch_ERA5!(dt, lat_ind, lon_ind, year, month;
-                    vars=vars, times=times, folder=folder);
+    @info "Downloading land data for year $(year)...";
+
+    # set up CDSAPI_CLIENT
+    global CDSAPI_CLIENT;
+    update_CDSAPI_password!();
+
+    # set lat and lon bounds
+    lat1 = lat_ind - 91;
+    lat2 = lat_ind - 90;
+    lon1 = lon_ind - 181;
+    lon2 = lon_ind - 180;
+
+    # file name to save the downloaded dataset
+    file_name = "$(folder)ERA5_LAND_$(year)_$(lat_ind)_$(lon_ind).nc";
+
+    # retrieve data if file does not exist
+    if !isfile(file_name)
+        CDSAPI_CLIENT.retrieve(
+            "reanalysis-era5-land",
+            Dict(
+                "format"   => "netcdf",
+                "variable" => vars,
+                "year"     => "$(year)",
+                "month"    => ["$(i)" for i in 1:12],
+                "day"      => ["$(i)" for i in 1:31],
+                "time"     => times,
+                "area"     => ["$(lat2)", "$(lon1)", "$(lat1)", "$(lon2)"],
+            ),
+            file_name
+        );
+    end
+
+    return nothing
+end
+
+
+
+
+function fetch_ERA5!(
+            dt::ERA5LandHourly,
+            year::Int;
+            vars::Array{String,1} = ERA5_LAND_SELECTION,
+            times::Array{String,1} = ERA5_LAND_TIMES,
+            folder::String = ""
+)
+    @info "Downloading land data for year $(year)...";
+
+    # set up CDSAPI_CLIENT
+    global CDSAPI_CLIENT;
+    update_CDSAPI_password!();
+
+    # file name to save the downloaded dataset
+    file_name = "$(folder)ERA5_LAND_$(year).nc";
+
+    # retrieve data if file does not exist
+    if !isfile(file_name)
+        CDSAPI_CLIENT.retrieve(
+            "reanalysis-era5-land",
+            Dict(
+                "format"   => "netcdf",
+                "variable" => vars,
+                "year"     => "$(year)",
+                "month"    => ["$(i)" for i in 1:12],
+                "day"      => ["$(i)" for i in 1:31],
+                "time"     => times,
+            ),
+            file_name
+        );
+    end
+
+    return nothing
+end
+
+
+
+
+function fetch_ERA5!(
+            dt::ERA5LandHourly,
+            year::Int,
+            per_item::Bool;
+            vars::Array{String,1} = ERA5_LAND_SELECTION,
+            times::Array{String,1} = ERA5_LAND_TIMES,
+            folder::String = ""
+)
+    @info "Downloading land data for year $(year) per item...";
+
+    # set up CDSAPI_CLIENT
+    global CDSAPI_CLIENT;
+    update_CDSAPI_password!();
+
+    for _item in vars
+        @info "Querying $(_item)";
+
+        # file name to save the downloaded dataset
+        file_name = "$(folder*_item)_LAND_$(year).nc";
+
+        # retrieve data if file does not exist
+        if !isfile(file_name)
+            CDSAPI_CLIENT.retrieve(
+                "reanalysis-era5-land",
+                Dict(
+                    "format"   => "netcdf",
+                    "variable" => _item,
+                    "year"     => "$(year)",
+                    "month"    => ["$(i)" for i in 1:12],
+                    "day"      => ["$(i)" for i in 1:31],
+                    "time"     => times,
+                ),
+                file_name
+            );
+        end
+    end
+
+    return nothing
+end
+
+
+
+
+function fetch_ERA5!(
+            dt::ERA5SingleLevelsHourly,
+            lat_ind::Int,
+            lon_ind::Int,
+            year::Int;
+            vars::Array{String,1} = ERA5_SINGLE_LEVELS_SELECTION,
+            times::Array{String,1} = ERA5_LAND_TIMES,
+            folder::String = ""
+)
+    @info "Downloading single levels data for year $(year)...";
+
+    # set up CDSAPI_CLIENT
+    global CDSAPI_CLIENT;
+    update_CDSAPI_password!();
+
+    # set lat and lon bounds
+    lat1 = lat_ind - 91;
+    lat2 = lat_ind - 90;
+    lon1 = lon_ind - 181;
+    lon2 = lon_ind - 180;
+
+    # file name to save the downloaded dataset
+    file_name = "$(folder)ERA5_SL_$(year)_$(lat_ind)_$(lon_ind).nc";
+
+    # retrieve data if file does not exist
+    if !isfile(file_name)
+        CDSAPI_CLIENT.retrieve(
+            "reanalysis-era5-single-levels",
+            Dict(
+                "product_type" => "reanalysis",
+                "format"       => "netcdf",
+                "variable"     => vars,
+                "year"         => "$(year)",
+                "month"        => ["$(i)" for i in 1:12],
+                "day"          => ["$(i)" for i in 1:31],
+                "time"         => times,
+                "area"         => ["$(lat2)", "$(lon1)", "$(lat1)", "$(lon2)"],
+            ),
+            file_name
+        );
+    end
+
+    return nothing
+end
+
+
+
+
+function fetch_ERA5!(
+            dt::ERA5SingleLevelsHourly,
+            year::Int;
+            vars::Array{String,1} = ERA5_SINGLE_LEVELS_SELECTION,
+            times::Array{String,1} = ERA5_LAND_TIMES,
+            folder::String = ""
+)
+    @info "Downloading single levels data for year $(year)...";
+
+    # set up CDSAPI_CLIENT
+    global CDSAPI_CLIENT;
+    update_CDSAPI_password!();
+
+    # file name to save the downloaded dataset
+    file_name = "$(folder)ERA5_SL_$(year).nc";
+
+    # retrieve data if file does not exist
+    if !isfile(file_name)
+        CDSAPI_CLIENT.retrieve(
+            "reanalysis-era5-single-levels",
+            Dict(
+                "product_type" => "reanalysis",
+                "format"       => "netcdf",
+                "variable"     => vars,
+                "year"         => "$(year)",
+                "month"        => ["$(i)" for i in 1:12],
+                "day"          => ["$(i)" for i in 1:31],
+                "time"         => times,
+            ),
+            file_name
+        );
+    end
+
+    return nothing
+end
+
+
+
+
+function fetch_ERA5!(
+            dt::ERA5SingleLevelsHourly,
+            year::Int,
+            per_item::Bool;
+            vars::Array{String,1} = ERA5_SINGLE_LEVELS_SELECTION,
+            times::Array{String,1} = ERA5_LAND_TIMES,
+            folder::String = ""
+)
+    @info "Downloading single levels data for year $(year) per item...";
+
+    # set up CDSAPI_CLIENT
+    global CDSAPI_CLIENT;
+    update_CDSAPI_password!();
+
+    for _item in vars
+        @info "Querying $(_item)";
+
+        # file name to save the downloaded dataset
+        file_name = "$(folder*_item)_SL_$(year).nc";
+
+        # retrieve data if file does not exist
+        if !isfile(file_name)
+            CDSAPI_CLIENT.retrieve(
+                "reanalysis-era5-single-levels",
+                Dict(
+                    "product_type" => "reanalysis",
+                    "format"       => "netcdf",
+                    "variable"     => _item,
+                    "year"         => "$(year)",
+                    "month"        => ["$(i)" for i in 1:12],
+                    "day"          => ["$(i)" for i in 1:31],
+                    "time"         => times,
+                ),
+                file_name
+            );
+        end
     end
 
     return nothing
