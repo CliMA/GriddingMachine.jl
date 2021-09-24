22346d73c39770a21f8c5311e66fd8d5ef2db958
Yujie Wang <jesiner@gmail.com>
2021-09-24 10:37:29 -0700

    switch to another serevr with public port

--- src/Requestor.jl
+++ src/Requestor.jl
@@ -18,7 +18,7 @@ function request_LUT end;
 
 
 """
-    request_LUT(artname::String, lat::Number, lon::Number, cyc::Int = 0; user::String="Anonymous", interpolation::Bool = false, server::String = "fluo.gps.caltech.edu", port::Int = 8848)
+    request_LUT(artname::String, lat::Number, lon::Number, cyc::Int = 0; user::String="Anonymous", interpolation::Bool = false, server::String = "tofu.gps.caltech.edu", port::Int = 5055)
 
 Request data from the server, given
 - `artname` Artifact full name such as `LAI_MODIS_2X_8D_2017_V1`
@@ -39,13 +39,13 @@ request_LUT("LAI_MODIS_2X_8D_2017_V1", 30.5, 115.5, 8);
 request_LUT("LAI_MODIS_2X_8D_2017_V1", 30.5, 115.5, 8; interpolation=true);
 ```
 """
-request_LUT(artname::String, lat::Number, lon::Number, cyc::Int = 0; user::String="Anonymous", interpolation::Bool = false, server::String = "fluo.gps.caltech.edu", port::Int = 8848) = (
+request_LUT(artname::String, lat::Number, lon::Number, cyc::Int = 0; user::String="Anonymous", interpolation::Bool = false, server::String = "tofu.gps.caltech.edu", port::Int = 5055) = (
     # make sure the artifact is within our collection
     _metas = load_artifacts_toml(joinpath(@__DIR__, "../Artifacts.toml"));
     _artns = [_name for (_name,_) in _metas];
     @assert artname in _artns;
 
-    # send a request to our webserver at fluo.gps.caltech.edu and translate it back to Dictionary
+    # send a request to our webserver at tofu.gps.caltech.edu:5055 and translate it back to Dictionary
     _response = get("http://$(server):$(port)/request.json?user=$(user)&artifact=$(artname)&lat=$(lat)&lon=$(lon)&cyc=$(cyc)&interpolate=$(interpolation)");
     _json_str = String(_response.body);
     _results  = parse(_json_str);
