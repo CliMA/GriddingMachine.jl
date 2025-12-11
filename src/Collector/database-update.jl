2be0df5750ee94b96a639762d2ef67d704385623
Yujie <jesiner@gmail.com>
2025-12-11 17:52:12 +0800

    make path when update it

--- src/Collector/database-update.jl
+++ src/Collector/database-update.jl
@@ -1,5 +1,12 @@
 """ Update the database of GriddingMachine.jl """
 function update_database!()
+    # make sure the GriddingMachine directory exists
+    GRIDDINGMACHINE_HOME = joinpath(homedir(), "GriddingMachine");
+    mkpath(GRIDDINGMACHINE_HOME);
+    mkpath(joinpath(GRIDDINGMACHINE_HOME, "cache"));
+    mkpath(joinpath(GRIDDINGMACHINE_HOME, "public"));
+
+    # download the latest database and reload it
     download_database!();
     global YAML_DATABASE, YAML_TAGS;
     (YAML_DATABASE, YAML_TAGS) = load_database!();
--- src/GriddingMachine.jl
+++ src/GriddingMachine.jl
@@ -1,13 +1,6 @@
 module GriddingMachine
 
 
-# make sure the GriddingMachine directory exists
-GRIDDINGMACHINE_HOME = joinpath(homedir(), "GriddingMachine");
-mkpath(GRIDDINGMACHINE_HOME);
-mkpath(joinpath(GRIDDINGMACHINE_HOME, "cache"));
-mkpath(joinpath(GRIDDINGMACHINE_HOME, "public"));
-
-
 # include the modules
 include("Collector/Collector.jl");
 include("Indexer/Indexer.jl");
