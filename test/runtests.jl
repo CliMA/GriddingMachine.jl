e858d2f1a6f4d52af63e538e3299ff31364cbbfc
Yujie Wang <jesiner@gmail.com>
2026-09-04 09:00:33 +0800

    cleaner tests (tutorials) and update the deps

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,35 +1,10 @@
 using GriddingMachine
-using GriddingMachine.Collector
-using GriddingMachine.Indexer
-using GriddingMachine.Requestor
-
-using HTTP
-using JSON
-using NetcdfIO: append_nc!, save_nc!
-using OrderedCollections: OrderedDict
-using SHA
 using Test
 
-include("fixtures.jl")
 
-@testset verbose = true "GriddingMachine" begin
-    @testset verbose = true "Collector" begin
-        include("collector.jl")
-    end
-
-    @testset "Indexer read_dataset" begin
-        include("indexer-read.jl")
-    end
-
-    @testset "Model input dictionaries" begin
-        include("model-inputs.jl")
-    end
-
-    @testset verbose = true "Land datasets and CO2" begin
-        include("land-datasets.jl")
-    end
-
-    @testset verbose = true "Grid dictionaries from tags" begin
-        include("grid-from-tags.jl")
-    end
-end
+# make sure the key features (data sharing) are working as expected
+@testset "GriddingMachine" verbose = true begin
+    include("collector.jl");
+    include("indexer.jl");
+    include("requestor.jl");
+end;
